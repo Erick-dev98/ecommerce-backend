491b228427c415c7777b63b7daa8033c99c4c7c7
erickmwei680@gmail.com <erickmwei680@gmail.com>
2024-02-25 17:43:36 +0300

    first commit

--- index.js
+++ index.js
@@ -200,6 +200,44 @@ app.post('/login',async (req,res)=>{
     }
 })
 
+// Creating endpoint for newcollection data
+app.get('/newcollections',async (req,res)=>{
+    let products = await Product.find({});
+    let newcollection = products.slice(1).slice(-8);
+    console.log("NewCollection Fetched");
+    res.send(newcollection);
+})
+
+// Creating endpoint for popular in women section
+app.get('/popularinwomen',async (req,res)=>{
+    let products = await Product.find({category:"women"});
+    let popular_in_women = products.slice(0,4);
+    console.log("Popular in women fetched");
+    res.send(popular_in_women);
+})
+
+// Creating middleware to fetch user
+const fetchUser = async (req,res,next)=>{
+    const token = req.header('auth-token');
+    if (!token) {
+        res.status(401).send({errors:"Please authenticate using valid token"})
+    }
+    else{
+        try {
+           const data = jwt.verify(token,'secret_ecom');
+           req.user = data.user;
+           next();
+        } catch (error) {
+            res.status(401).send({errors:"please aunthenticate using a valid token"})
+        }
+    }
+}
+
+// Creating endpoint for adding products in cartdata 
+app.post('/addtocart',fetchUser,async (req,res)=> {
+    console.log(req.body,req.user);
+})
+
 app.listen(port,(error)=>{
     if (!error) {
         console.log("Server Running on Port "+port)
